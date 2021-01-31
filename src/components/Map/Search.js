1a3e9196530570e903e2e3bd6e0771f15d11eba6
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2021-01-31 15:09:48 -0500

    Add Search bar for map

---
+++ src/components/Map/Search.js
@@ -0,0 +1,77 @@
+import React from 'react'
+
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption,
+  } from "@reach/combobox"
+  import usePlacesAutocomplete, {
+    getGeocode,
+    getLatLng,
+  } from "use-places-autocomplete"
+
+
+
+// Search box --- try and put in separate file --- move off of logo
+function Search({ panTo }) {
+    const {
+      ready,
+      value,
+      suggestions: { status, data },
+      setValue,
+      clearSuggestions,
+    } = usePlacesAutocomplete({
+      requestOptions: {
+        location: {
+          lat: () => 45.815010,
+          lng: () => 15.981919
+        },
+        //how much to zoom in
+        radius: 200 * 1000,
+      },
+    })
+    return (
+       
+      <div className="search" >
+        <Combobox
+          onSelect={async (address) => {
+            setValue(address, false);
+            clearSuggestions();
+  
+            try {
+              const results = await getGeocode({ address });
+              const { lat, lng } = await getLatLng(results[0])
+              panTo({ lat, lng })
+  
+            } catch (error) {
+              console.log("error!")
+            }
+            console.log(address);
+          }}
+        >
+          
+          <ComboboxInput
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value)
+            }}
+            disabled={!ready}
+            placeholder="Enter an Address"
+          />
+          <ComboboxPopover>
+            <ComboboxList>
+              {status === "OK" && data.map(({ id, description }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+            </ComboboxList>
+          </ComboboxPopover>
+        </Combobox>
+      </div >
+    )
+  }
+  
+  export default Search
+  
+  
